3200a9917cd5e3ad8ead0e3e6f8f53acbfc91587
Ana Otero <anajova711@gmail.com>
2024-02-03 14:00:09 -0500

    fashion packages

--- src/components/fashion/fashion.js
+++ src/components/fashion/fashion.js
@@ -21,9 +21,123 @@ const Fashion = () => {
   }, []);
 
   return (
-    <div>
+    <div className='fashion_container'>
       <h2>Fashion Gallery</h2>
-      <div id='fashionphotos'>
+      <div className='fashion_packages'>
+        <div id='package1'>
+          <div className='innerbox_package'>
+            <img
+              alt=''
+              style={{ height: "450px", width: "300px" }}
+              src={`https://ieqxnbaivrturiczktvu.supabase.co/storage/v1/object/public/fashionphotos/${images[2]?.name}`}
+            ></img>
+            <p>Model Test 1</p>
+            <p>$600</p>
+            <ul>
+              <li>1 hour photo shoot.</li>
+              <li>Professional Hair and Make up artist.</li>
+              <li>Up to 2 outfits.</li>
+              <li>6 professionally retouched final images.</li>
+              <li>10 color-corrected additional images.</li>
+              <li>Professional editing and color grading of all images.</li>
+              <li>Final images delivered digitally in just few days.</li>
+              <li>
+                Behind the scene video for your social media, upon request.
+              </li>
+            </ul>
+            <button>Purchase</button>
+          </div>
+        </div>
+        <div id='package2'>
+          <div className='innerbox_package'>
+            <img
+              alt=''
+              style={{ height: "450px", width: "300px" }}
+              src={`https://ieqxnbaivrturiczktvu.supabase.co/storage/v1/object/public/fashionphotos/${images[1]?.name}`}
+            ></img>
+            <p>Model Test 2</p>
+            <p>$850</p>
+            <ul>
+              <li>2 hour photo shoot.</li>
+              <li>Studio or on location photo shoot.</li>
+              <li>Professional Hair and Make up artist.</li>
+              <li>Up to 4 outfits.</li>
+              <li>10 professionally retouched final images.</li>
+              <li>15 color-corrected additional images.</li>
+              <li>Professional editing and color grading of all images.</li>
+              <li>Final images delivered digitally in just few days.</li>
+            </ul>
+            <button>Purchase</button>
+          </div>
+        </div>
+        <div id='package3'>
+          <div className='innerbox_package'>
+            <img
+              alt=''
+              style={{ height: "450px", width: "300px" }}
+              src={`https://ieqxnbaivrturiczktvu.supabase.co/storage/v1/object/public/fashionphotos/${images[5]?.name}`}
+            ></img>
+            <p>Model Test 3</p>
+            <p>$1,250</p>
+            <ul>
+              <li>Up to 4 hour photo day. Half a day.</li>
+              <li>Studio or on location photo shoot.</li>
+              <li>2 looks with professional hair and make up artist.</li>
+              <li>Up to 6 outfits.</li>
+              <li>20 professionally retouched final images.</li>
+              <li>60 color-corrected additional images.</li>
+              <li>Professional editing and color grading of all images.</li>
+              <li>Final images delivered digitally in just few days.</li>
+              <li>
+                Behind the scene video for your social media, upon request.
+              </li>
+            </ul>
+            <button>Purchase</button>
+          </div>
+        </div>
+        <div id='package4'>
+          <div className='innerbox_package'>
+            <img
+              alt=''
+              style={{ height: "450px", width: "300px" }}
+              src={`https://ieqxnbaivrturiczktvu.supabase.co/storage/v1/object/public/fashionphotos/${images[3]?.name}`}
+            ></img>
+            <p>Model Test 4</p>
+            <p>$2,600</p>
+            <ul>
+              <li>Up to 8 hour photo shoot day. Full day.</li>
+              <li>Studio or on location photo shoot.</li>
+              <li>4 looks with professional hair and make up artist.</li>
+              <li>Wardrobe stylist.</li>
+              <li>Unlimited outfits.</li>
+              <li>40 professionally retouched final images.</li>
+              <li>All selected color-corrected images.</li>
+              <li>Professional editing and color grading of all images.</li>
+              <li>Final images delivered digitally in just few days.</li>
+              <li>
+                Behind the scene video for your social media, upon request.
+              </li>
+            </ul>
+            <button>Purchase</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>
+          Even though every package includes hair and make up artist services,
+          and we recommend it for best result, it is possible to exclude this
+          service from your package, upon request.
+        </p>
+        <p>Just email us at quote@creativecaptureph.com</p>
+      </div>
+      <div>
+        <p>
+          We understand that every project is unique. Email us a few words about
+          your project and deadlines to get your quote at
+          quote@creativecaptureph.com
+        </p>
+      </div>
+      {/* <div id='fashionphotos'>
         {images.map((image) => (
           <img
             key={image.id}
@@ -32,7 +146,7 @@ const Fashion = () => {
             src={`https://ieqxnbaivrturiczktvu.supabase.co/storage/v1/object/public/fashionphotos/${image.name}`}
           />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
